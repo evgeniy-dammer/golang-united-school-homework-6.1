aa44a0b529b4abdca709b5b29cfba9e0464c340d
Evgeniy Dammer <evgeniydammer@gmail.com>
2022-06-02 09:56:29 +0500

    Add solution
    Add solution

--- reactangle.go
+++ reactangle.go
@@ -5,3 +5,10 @@ type Rectangle struct {
 	Height, Weight float64
 }
 
+func (rectangle Rectangle) CalcPerimeter() float64 {
+	return (2 * rectangle.Height) + (2 * rectangle.Weight)
+}
+
+func (rectangle Rectangle) CalcArea() float64 {
+	return rectangle.Height * rectangle.Weight
+}
